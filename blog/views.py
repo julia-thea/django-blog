22e63d111b636eddb79be3d41e0ab6b273db4bc8
Julia Thea <yuliya.boyadjan@gmail.com>
2020-04-20 12:59:29 -0500

    updated views

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-posts = [
-  {
-    'author': 'CoreyMS',
-    'title': "Blog Post 1",
-    'content': 'First post content',
-    'date_posted': 'August 27 2020'
-  },
-    {
-    'author': 'Jane Doe',
-    'title': "Blog Post 1",
-    'content': 'First post content',
-    'date_posted': 'August 28 2020'
-  }
-
-]
+from .models import Post
 
 def home(request):
   context = {
-    'posts': posts
+    'posts': Post.objects.all()
   }
   return render(request, 'blog/home.html', context)
 
