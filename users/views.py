a375075b99fd22690fd7b094dd181ea9275a0e2e
Julia Thea <yuliya.boyadjan@gmail.com>
2020-04-28 10:33:49 -0500

    modified user views

--- users/views.py
+++ users/views.py
@@ -13,3 +13,6 @@ def register(request):
   else:
     form = UserRegisterForm()
   return render(request, 'users/register.html', {'form': form})
+
+  def profile(request):
+    return render(request, 'users/profile.html')
