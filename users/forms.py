582f2396a9afa006c60750ca6bb23a3c21635081
Julia Thea <yuliya.boyadjan@gmail.com>
2020-04-29 08:34:56 -0500

    more forms stuff

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
   email = forms.EmailField()
@@ -14,3 +15,8 @@ class UserUpdateForm(forms.ModelForm):
 
   class Meta:
     fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+  class Meta:
+    model = Profilefield = ['image']
+
