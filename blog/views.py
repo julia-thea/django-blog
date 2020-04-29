691bfe65a986cc932f4e58850d4d7f6d9f0ac9db
Julia Thea <yuliya.boyadjan@gmail.com>
2020-04-29 13:29:43 -0500

    more stuff

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 def home(request):
@@ -6,6 +7,12 @@ def home(request):
     'posts': Post.objects.all()
   }
   return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['date_posted']
 
 def about(request):
   return render(request, 'blog/about.html', {'title': 'About'})
