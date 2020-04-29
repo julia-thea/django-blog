e70b5105607962fd430c7216f1201fc0b599f415
Julia Thea <yuliya.boyadjan@gmail.com>
2020-04-29 17:01:51 -0500

    added classes for list view and detail view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 def home(request):
@@ -12,7 +12,10 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
-    ordering = ['date_posted']
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
 
 def about(request):
   return render(request, 'blog/about.html', {'title': 'About'})
