3ac9803ffb392b16d747d1974040779e5409961e
Julia Thea <yuliya.boyadjan@gmail.com>
2020-05-05 13:34:44 -0500

    misc

--- blog/models.py
+++ blog/models.py
@@ -13,5 +13,5 @@ class Post(models.Model):
   def __str__(self):
     return self.title
 
-  def get_absolute_url(self):
-    return reverse('post-detail', kwargs={'pk': self.pk})  
+  def get_absolute_url(self, *args, **kawrgs):
+    return reverse('post-detail', kawrgs={'pk': self.pk})  
